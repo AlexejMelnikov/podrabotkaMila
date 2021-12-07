b5c53a74190a19bb93eb82324e585b8d577abae4
AlexejMelnikov <34479105+AlexejMelnikov@users.noreply.github.com>
2021-12-07 22:20:52 +0300

    Add files via upload

---
+++ JS/scrript.js
@@ -0,0 +1,12 @@
+let study = () => {
+  console.log("hi");
+  let studyTable = Document.querySelector(".study-list");
+  console.log(studyTable);
+};
+// let userInput = prompt("Insert data for update queue");
+const showStudy = () => {
+  console.log(Document);
+  // let studyTable = Document.querySelector(".study-list");
+  console.log(studyTable);
+  studyTable.style.display = "none";
+};
